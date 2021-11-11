3042e6582c088dfd849bb9c4caf2f4d66e089d40
Daniel Sapkota <daniel@waterbot.xyz>
2021-11-10 18:43:23 -0800

    Done

--- script.py
+++ script.py
@@ -15,9 +15,9 @@ def round_down(x, divNumber):
 
 parser = argparse.ArgumentParser("Grid Trader")
 parser.add_argument("--divNumber", help="Divisible price points to check at", type=int, default=10)
-parser.add_argument("--maxOrder", help="Size of single order", type=int, default=10)
-parser.add_argument("--orderAbove", help="Number of orders above current price", type=int, default=20)
-parser.add_argument("--orderBelow", help="Number of orders below current price", type=int, default=20)
+parser.add_argument("--maxOrder", help="Size of single order", type=int, default=1)
+parser.add_argument("--orderAbove", help="Number of orders above current price", type=int, default=50)
+parser.add_argument("--orderBelow", help="Number of orders below current price", type=int, default=50)
 parser.add_argument("--startPrice", help="Only starts if the current price is in the given range inputted here", type=int, default=-1)
 parser.add_argument("--sleepTime", help="Seconds delayed", type=int, default=60)
 
@@ -72,7 +72,7 @@ def perform_once(reset=False):
             add_order(otype, params['maxOrder'], i)
             count = count + 1
 
-            if count % 40 == 0:
+            if count % 10 == 0:
                 time.sleep(1)
 
     print(time.time() - start_time)
