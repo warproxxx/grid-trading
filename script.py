4375442868235676457ed70aeb06aa968fdad41a
Daniel Sapkota <daniel@waterbot.xyz>
2021-11-10 16:52:20 -0800

    Fixing

--- script.py
+++ script.py
@@ -22,9 +22,23 @@ parser.add_argument("--startPrice", help="Only starts if the current price is in
 parser.add_argument("--sleepTime", help="Seconds delayed", type=int, default=60)
 
 params = vars(parser.parse_args())
+startPrice = params['startPrice']
 
 lt = liveTrading(lev=25)
 lt.set_leverage()
+
+obook = lt.get_orderbook()
+
+if startPrice == -1:
+    startPrice = obook['best_bid']
+
+curr_up = int(round_up(startPrice, params['divNumber']))
+curr_down = int(round_down(startPrice, params['divNumber']))
+
+above_points = [i for i in range(curr_up, int(curr_up+((params['orderAbove']) * params['divNumber'])), params['divNumber'])]
+below_points = [i for i in range(curr_down, int(curr_down-((params['orderBelow']) * params['divNumber'])), params['divNumber'] * -1)]
+all_points = below_points + above_points
+print(all_points)
 
 def add_order(order_type, amount, price):
     t = threading.Thread(target=(lt.limit_trade), args=(order_type, amount, price,))
@@ -37,59 +51,32 @@ def remove_order(order_id):
 def perform_once(reset=False):
     obook = lt.get_orderbook()
 
-    curr_up = int(round_up(obook['best_bid'], params['divNumber']))
-    curr_down = int(round_down(obook['best_bid'], params['divNumber']))
+    orders_df = pd.DataFrame(lt.get_all_orders())
 
-    if params['startPrice'] == -1 or (params['startPrice'] >= curr_down and params['startPrice'] <= curr_up):
+    start_time = time.time()
 
-        if reset == True:
-            lt.close_all_orders()
+    if len(orders_df) > 0:
+        orders = orders_df[['price', 'order_id']].set_index('price').T.to_dict()
+    else:
+        orders = {}
 
-        orders_df = pd.DataFrame(lt.get_all_orders())
+    print(orders)
 
-        start_time = time.time()
+    for i in all_points:
+        if i not in orders:
+            if i > obook['best_bid']:
+                otype = 'sell'
+            else:
+                otype = 'buy'
+            add_order(otype, params['maxOrder'], i)
 
-        if len(orders_df) > 0:
-            orders = orders_df[['price', 'order_id']].set_index('price').T.to_dict()
-        else:
-            orders = {}
-
-        print("Starting at {} {}".format(curr_up, curr_down))
-
-        above_points = [i for i in range(curr_up, int(curr_up+((params['orderAbove']) * params['divNumber'])), params['divNumber'])]
-        below_points = [i for i in range(curr_down, int(curr_down-((params['orderBelow']) * params['divNumber'])), params['divNumber'] * -1)]
-
-        for order in orders:
-            if int(order) not in above_points and int(order) not in below_points:
-
-                print("Removing {}".format(order))
-                remove_order(orders[order]['order_id'])
-
-
-        for i in above_points:
-            if i not in orders:
-                add_order('sell', params['maxOrder'], i)
-
-        for i in below_points:
-            if i not in orders:
-                add_order('buy', params['maxOrder'], i)
-
-
-
-        print(time.time() - start_time)
+    print(time.time() - start_time)
 
 def perform_all():
-    curr_count = 0
 
     while True:
-        if (curr_count == 60):
-            curr_count = 0
-            perform_once(reset=True)
-        else:
-            perform_once()
-
+        perform_once()
         time.sleep(params['sleepTime'])
-        curr_count = curr_count + 1
 
 if __name__ == "__main__":
     perform_all()
