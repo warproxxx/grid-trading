553bdd518839bfa0ac73a8ab2c808e049048b797
Daniel Sapkota <daniel@waterbot.xyz>
2021-11-18 13:43:01 -0800

    Readme

--- README.md
+++ README.md
@@ -3,13 +3,10 @@
 2) screen -r -d grid
 ![Screenshot](ss.png)
 
-Sample Input:
-python3 script.py --divNumber 10 —maxOrder 100 —orderAbove 50 —orderBelow 50 —startPrice 59995 --sleepTime 60
-
-If —startPrice is not set or set to -1, current price will be used
+**Sample Input:**
+```python3 main.py --maxPositionSize 500 --sizePerOrder 10 --timeframe 10000000 --sleepTime 15 --divNumber 10```
 
 To detach:
-
 3) Ctrl A + d
 
 
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 
 parser = argparse.ArgumentParser("script.py")
     
-parser.add_argument("--maxPositionSize", help="Max Position Size", type=int, default=50)
-parser.add_argument("--sizePerOrder", help="Size per order", type=int, default=1)
+parser.add_argument("--maxPositionSize", help="Max Position Size", type=int, default=500)
+parser.add_argument("--sizePerOrder", help="Size per order", type=int, default=10)
 parser.add_argument("--timeframe", help="Run time in miniutes", type=int, default=240)
 parser.add_argument("--sleepTime", help="Seconds delayed", type=int, default=15)
 parser.add_argument("--divNumber", help="Divisible Price points to check at", type=int, default=10)
