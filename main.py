2ccfd16a54a080a88770454c31e47ffb91a3f915
Daniel Sapkota <daniel@waterbot.xyz>
2021-11-18 12:26:30 -0800

    Mostly done

---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+from utils import gridTrader
+
+parser = argparse.ArgumentParser("script.py")
+
+parser.add_argument("--maxPositionSize", help="Max Position Size", type=int, default=500)
+parser.add_argument("--sizePerOrder", help="Size per order", type=int, default=10)
+parser.add_argument("--orderDivNumber", help="Divisible price points to check at", type=int, default=25)
+parser.add_argument("--timeframe", help="Run time in miniutes", type=int, default=240)
+parser.add_argument("--sleepTime", help="Seconds delayed", type=int, default=15)
+parser.add_argument("--divNumber", help="Divisible Price points to check at", type=int, default=10)
+
+
+gt = gridTrader(vars(parser.parse_args()))
+params = gt.get_processed_vars()
+
+def perform_once():
+    currentSize = gt.getPositionSize()
+    sizeDiff =  params['maxPositionSize'] - currentSize
+
+    if sizeDiff > 0  and gt.checkSleep():
+        gt.setOrders()
+        orderPriceArray = gt.getLongOrderPriceArray(sizeDiff)
+
+        for eachPrice in orderPriceArray:
+            if gt.notOrderAlreadyPlaced(eachPrice):
+                gt.placeOrder('buy', params['sizePerOrder'], eachPrice)
+
+    if sizeDiff > 0  and gt.checkSleep():
+        orderPriceArray = gt.getShortOrderPriceArray(currentSize)
+
+        for eachPrice in orderPriceArray:
+            if gt.notOrderAlreadyPlaced(eachPrice):
+                gt.placeOrder('sell', params['sizePerOrder'], eachPrice)
+
+def perform_all():
+
+    while True:
+        perform_once()
+        time.sleep(params['sleepTime'])
+
+if __name__ == "__main__":
+    perform_all()
---
+++ utils.py
@@ -0,0 +1,75 @@
+from liveTrader import liveTrading
+import time
+import pandas as pd
+import threading
+import math
+
+class gridTrader():
+    def __init__(self, params):
+        self.params = params
+        self.lt = liveTrading(lev=25)
+        self.lt.set_leverage()
+
+    def get_processed_vars(self):
+        params['numberOfOrders'] = int(params['maxPositionSize']/params['sizePerOrder'])
+        startTime = int(time.time())
+
+        params['endTime'] = -1
+
+        if params['timeframe'] != -1:
+            params['endTime'] = startTime + 60 * params['timeframe']
+
+        self.params = params
+        return params
+
+    def getPositionSize(self):
+        return lt.get_position()[2]
+
+    def checkSleep(self):
+        if self.params['endTime'] == -1:
+            return True
+        else:
+            if time.time() < self.params['endTime']:
+                return True
+            else:
+                return False
+
+    def getLongOrderPriceArray(self, totalSize):
+        currPrice = self.lt.get_orderbook()['best_ask']
+        curr_down = int(round_down(currPrice, self.params['divNumber']))
+        numberOfOrders = (totalSize/self.params['divNumber']) + 1
+        below_points = [curr_down - (i * self.params['divNumber']) for i in range(numberOfOrders)]
+        return below_points
+
+    def getShortOrderPriceArray(self, totalSize):
+        currPrice = self.lt.get_orderbook()['best_bid']
+        curr_up = int(round_up(currPrice, self.params['divNumber']))
+        numberOfOrders = (totalSize/self.params['divNumber']) + 1
+        above_points = [curr_up + (i * self.params['divNumber']) for i in range(numberOfOrders)]
+        return above_points
+
+    def setOrders(self):
+        self.orders = {}
+        orders_df = pd.DataFrame(self.lt.get_all_orders())
+        
+        if len(orders_df) > 0:
+            self.orders = orders_df[['price', 'order_id']].set_index('price').T.to_dict()
+        
+
+
+    def notOrderAlreadyPlaced(self, price):
+        if price not in self.orders:
+            return True
+        else:
+            return False
+
+    def placeOrder(self, order_type, amount, price):
+        t = threading.Thread(target=(self.lt.limit_trade), args=(order_type, amount, price,))
+        t.start()
+
+def round_up(x, divNumber):
+    return int(math.ceil(x / divNumber)) * divNumber
+
+def round_down(x, divNumber):
+    return int(math.floor(x / divNumber)) * divNumber
+    
