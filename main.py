c95dc95114d2e3f50953304af2ad9c86f7bbee41
Daniel Sapkota <daniel@waterbot.xyz>
2021-11-18 20:27:32 -0800

    rm useless

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def perform_once():
 
         if sizeDiff >= 0  and gt.checkSleep():
             openOrderPriceArray = gt.getOpenOrderPriceArray(sizeDiff)
-            print("Open Array: {}".format(openOrderPriceArray))
 
             for eachPrice in openOrderPriceArray:
                 if gt.notOrderAlreadyPlaced(eachPrice):
@@ -43,7 +42,6 @@ def perform_once():
                     
         if currentSize > 0  and gt.checkSleep():
             closeOrderPriceArray = gt.getCloseOrderPriceArray(currentSize)
-            print("Close Array: {}".format(closeOrderPriceArray))
 
             for eachPrice in closeOrderPriceArray:
                 if gt.notOrderAlreadyPlaced(eachPrice):
